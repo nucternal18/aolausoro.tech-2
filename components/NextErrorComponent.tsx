fabb8f5c222f0f921d1f4897f1cce42a7e84a268
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-11-22 21:55:49 +0000

    22.11.21. fixed issue in lib/getUser and modified _error.tsx

--- components/NextErrorComponent.tsx
+++ components/NextErrorComponent.tsx
@@ -1,15 +1,22 @@
+import { Layout } from "./layout";
+
 export default function NextErrorComponent({ statusCode }) {
-    return (
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    )
-  }
+  return (
+    <Layout title={`${statusCode} error on server`}>
+      <div className="flex flex-col items-center mt-20">
+        <h1 className="my-5 text-6xl">Whoops!</h1>
+        <h2 className="mb-5 text-4xl text-gray-500">
+          {statusCode
+            ? `A ${statusCode} error occurred on server`
+            : "An error occurred on client"}
+        </h2>
+      </div>
+    </Layout>
+  );
+}
 
 NextErrorComponent.getInitialProps = async ({ res, err }) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    const hasGetInitialPropsRun = true
-  return { statusCode, hasGetInitialPropsRun }
-}
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  const hasGetInitialPropsRun = true;
+  return { statusCode, hasGetInitialPropsRun };
+};
--- lib/getUser.ts
+++ lib/getUser.ts
@@ -1,13 +1,24 @@
-import { DecodedIdToken, getAuth } from 'firebase-admin/auth';
-import nookies from 'nookies';
-import { defaultFirestore } from './firebaseAdmin';
+import {  getAuth } from 'firebase-admin/auth';
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+if (getApps().length === 0) {
+    initializeApp({
+        credential: cert({
+            privateKey: process.env.PRIVATE_KEY,
+            clientEmail: process.env.CLIENT_EMAIL,
+            projectId: process.env.PROJECT_ID,
+        }),
+        databaseURL: process.env.DATABASE_URL,
+    });
+}
 
 export default async function getUser(token): Promise<{ user: any;  }> {
     
     let userData;
     const userToken = await getAuth().verifyIdToken(token);
 
-    const userRef = defaultFirestore.collection('users').doc(userToken.uid);
+    const userRef = getFirestore().collection('users').doc(userToken.uid);
     const snapshot = await userRef.get();
     snapshot.exists ? (userData = snapshot.data()) : (userData = null);
     
