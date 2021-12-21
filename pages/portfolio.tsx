5fc80fadbd237250bbc51024383bfcfc9cfc17ae
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-12-21 16:50:46 +0000

    fix issue with projects not loading on portfolio page

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { FaGithub, FaChevronRight } from "react-icons/fa";
 import Link from "next/link";
+import { GetServerSidePropsContext } from "next";
 
 // Components
 import { Layout } from "../components/layout";
@@ -13,9 +14,8 @@ import { NEXT_URL } from "../config";
 
 // Context
 import { usePortfolio } from "../context/portfolioContext";
-import Loader from "components/Loader";
 
-const Portfolio = (props) => {
+const Portfolio = ({ projects }) => {
   const { state } = usePortfolio();
 
   return (
@@ -38,81 +38,96 @@ const Portfolio = (props) => {
         </h2>
       </section>
       <section className="max-w-screen-lg mx-auto mb-4">
-        {state.loading ? (
-          <div className="mx-auto w-full h-full py-10">
-            <Loader classes="w-12 h-12" />
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 gap-3 px-4 my-4 sm:grid-cols-2 md:grid-cols-3 sm:px-0">
-            {state.projects &&
-              state.projects.map((doc) => {
-                return (
-                  <Card key={doc.id}>
-                    <div>
-                      <Image
-                        src={doc.url}
-                        alt="project"
-                        layout="intrinsic"
-                        width={500}
-                        height={350}
-                        quality={50}
-                        className="overflow-hidden"
-                      />
-                    </div>
-                    <CardBody>
-                      <div className="flex flex-col justify-evenly-between">
-                        <div className="flex-1">
-                          <div className="mb-2">
-                            <CardTitle className="my-2 text-xl text-center">
-                              {doc.projectName}
-                            </CardTitle>
-                            <div className="w-1/4 mx-auto border-b-2 border-yellow-400"></div>
-                          </div>
-                          {/* <div className='w-full mb-2'>
+        <div className="grid grid-cols-1 gap-3 px-4 my-4 sm:grid-cols-2 md:grid-cols-3 sm:px-0">
+          {projects &&
+            projects.map((doc) => {
+              return (
+                <Card key={doc.id}>
+                  <div>
+                    <Image
+                      src={doc.url}
+                      alt="project"
+                      layout="intrinsic"
+                      width={500}
+                      height={350}
+                      quality={50}
+                      className="overflow-hidden"
+                    />
+                  </div>
+                  <CardBody>
+                    <div className="flex flex-col justify-evenly-between">
+                      <div className="flex-1">
+                        <div className="mb-2">
+                          <CardTitle className="my-2 text-xl text-center">
+                            {doc.projectName}
+                          </CardTitle>
+                          <div className="w-1/4 mx-auto border-b-2 border-yellow-400"></div>
+                        </div>
+                        {/* <div className='w-full mb-2'>
                           <p className='text-sm text-center'>
                             {doc?.description}
                           </p>
                         </div> */}
-                        </div>
+                      </div>
 
-                        <div className="flex items-center mx-auto my-2">
-                          {doc.techStack?.map((tech, idx) => (
-                            <div
-                              key={idx}
-                              className="mr-2 text-sm text-blue-400"
-                            >
-                              {tech}
-                            </div>
-                          ))}
-                        </div>
-
-                        <div className="my-2 ">
-                          <div className="flex items-center justify-between">
-                            <a
-                              href={doc.address}
-                              className="px-2 py-1 text-gray-200 bg-gray-400 rounded-full"
-                            >
-                              Live Preview
-                            </a>
-                            <a
-                              href={doc.github}
-                              className="flex items-center px-2 py-1 text-gray-200 bg-gray-400 rounded-full"
-                            >
-                              <FaGithub className="mr-1" />
-                              <span>Source Code</span>
-                            </a>
+                      <div className="flex items-center mx-auto my-2">
+                        {doc.techStack?.map((tech, idx) => (
+                          <div key={idx} className="mr-2 text-sm text-blue-400">
+                            {tech}
                           </div>
+                        ))}
+                      </div>
+
+                      <div className="my-2 ">
+                        <div className="flex items-center justify-between">
+                          <a
+                            href={doc.address}
+                            className="px-2 py-1 text-gray-200 bg-gray-400 rounded-full"
+                          >
+                            Live Preview
+                          </a>
+                          <a
+                            href={doc.github}
+                            className="flex items-center px-2 py-1 text-gray-200 bg-gray-400 rounded-full"
+                          >
+                            <FaGithub className="mr-1" />
+                            <span>Source Code</span>
+                          </a>
                         </div>
                       </div>
-                    </CardBody>
-                  </Card>
-                );
-              })}
-          </div>
-        )}
+                    </div>
+                  </CardBody>
+                </Card>
+              );
+            })}
+        </div>
       </section>
     </Layout>
   );
+};
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const res = await fetch(`${NEXT_URL}/api/projects`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { projects: data },
+  };
 };
 
 export default Portfolio;
