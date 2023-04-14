a0207333cb5db4bacb9985cdf9abb954684570ea
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-04-14 23:18:06 +0100

    modified projects page.

--- app/admin/projects/page.tsx
+++ app/admin/projects/page.tsx
@@ -7,9 +7,9 @@ import { useGetProjectsQuery } from "app/GlobalReduxStore/features/projects/proj
 import { ProjectProps } from "lib/types";
 
 function Projects() {
-  const { data: projects, isLoading } = useGetProjectsQuery();
+  const { data: projects, isLoading, isFetching } = useGetProjectsQuery();
 
-  if (isLoading) {
+  if (isFetching) {
     return (
       <section className="w-full h-full flex items-center justify-center">
         <Loader classes="w-8 h-8" />
@@ -18,7 +18,7 @@ function Projects() {
   }
   return (
     <section className="w-full h-full py-10 md:px-8">
-      <Table data={projects as ProjectProps[]} />
+      <Table data={(projects as ProjectProps[]) || []} />
     </section>
   );
 }
