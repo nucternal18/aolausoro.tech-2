ceda97eaded06ff232635eb6ae9f926fd83eb2b3
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-04-25 21:41:34 +0100

    modified: .github/workflows/node.js.yml app/portfolio/page.tsx components/Footer.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import { FaGithub, FaChevronRight } from "react-icons/fa";
 import Link from "next/link";
 import Loader from "components/Loader";
@@ -22,17 +23,18 @@ const Portfolio = () => {
     );
 
   return (
-    <section className="h-screen">
+    <section className="h-screen py-4">
       <section className="max-w-screen-lg mx-auto mb-4 px-4 lg:px-0">
         <div className="relative flex items-center justify-between mb-6 border-b border-current dark:border-yellow-500">
-          <h1 className="my-4 text-5xl font-thin text-center text-black dark:text-yellow-500">
+          <h1 className="my-4 text-2xl md:text-5xl font-thin text-center text-black dark:text-yellow-500">
             PORTFOLIO
           </h1>
           <Link
             href="https://github.com/nucternal18?tab=repositories"
-            className="px-2 py-2 flex items-center font-semibold text-gray-700 bg-transparent border border-gray-500 rounded hover:text-white dark:text-yellow-500 dark:border-yellow-500 hover:bg-gray-500 dark:hover:bg-yellow-500 dark:hover:text-white hover:border-transparent"
+            className="px-2 py-2 flex items-center font-semibold text-gray-700 bg-transparent border gap-1 border-gray-500 rounded hover:text-white dark:text-yellow-500 dark:border-yellow-500 hover:bg-gray-500 dark:hover:bg-yellow-500 dark:hover:text-white hover:border-transparent"
           >
-            <span>View GitHub</span>
+            <FaGithub />
+            <span className="text-xs sm:text-md md:text-lg">View GitHub</span>
             <FaChevronRight />
           </Link>
         </div>
@@ -42,12 +44,14 @@ const Portfolio = () => {
         </h2>
       </section>
       <section className="relative max-w-screen-lg mx-auto px-4 lg:px-0 mb-4">
-        <div className="grid grid-cols-1 gap-3 px-4 my-4 sm:grid-cols-2 md:grid-cols-3 sm:px-0">
-          {projects &&
-            publishedProjects?.map((doc) => (
-              <PortfolioCard key={doc.id} doc={doc} />
-            ))}
-        </div>
+        <Suspense>
+          <div className="grid grid-cols-1 gap-3 px-4 my-4 sm:grid-cols-2 md:grid-cols-3 sm:px-0">
+            {projects &&
+              publishedProjects?.map((doc) => (
+                <PortfolioCard key={doc.id} doc={doc} />
+              ))}
+          </div>
+        </Suspense>
       </section>
     </section>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <footer className="relative bottom-0 min-w-screen">
+    <footer className="relative min-w-screen">
       <div className="w-full p-6 mb-0 text-current bg-gray-100 dark:bg-gray-900 dark:text-yellow-500">
         <div className="container z-50 mx-auto my-4 text-center">
           <p className="z-50">Copyright© 2019 aolausoro.tech</p>
