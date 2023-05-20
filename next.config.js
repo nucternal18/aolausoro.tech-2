a135fbdd90824090838e37809e2bcb4caf62856a
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2023-05-20 17:26:22 +0100

    updated next.config and created mdx-components.tsx

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require("@sentry/nextjs");
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
   dest: "public",
@@ -14,10 +13,11 @@ const withPWA = require("next-pwa")({
   buildExcludes: [/middleware-manifest\.json$/],
 });
 
-module.exports = withPWA({
+const nextConfig = {
   // Your existing module.exports
   experimental: {
     appDir: true,
+    mdxRs: true,
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
   webpack: (config, { isServer }) => {
@@ -52,15 +52,6 @@ module.exports = withPWA({
   eslint: {
     dirs: ["app", "utils", "components"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
-  // sentry: {
-  //   // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-  //   // for client-side builds. (This will be the default starting in
-  //   // `@sentry/nextjs` version 8.0.0.) See
-  //   // https://webpack.js.org/configuration/devtool/ and
-  //   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-  //   // for more information.
-  //   hideSourceMaps: true,
-  // },
   env: {
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API,
     NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
@@ -68,20 +59,7 @@ module.exports = withPWA({
       process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     NEXT_PUBLIC_SITE_KEY: process.env.NEXT_PUBLIC_SITE_KEY,
   },
-});
+};
 
-// const sentryWebpackPluginOptions = {
-//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
-//   // the following options are set automatically, and overriding them is not
-//   // recommended:
-//   //   release, url, org, project, authToken, configFile, stripPrefix,
-//   //   urlPrefix, include, ignore
-
-//   silent: true, // Suppresses all logs
-//   // For all available options, see:
-//   // https://github.com/getsentry/sentry-webpack-plugin#options.
-// };
-
-// // Make sure adding Sentry options is the last code to run before exporting, to
-// // ensure that your source maps include changes from all other Webpack plugins
-// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+const withMDX = require("@next/mdx")();
+module.exports = withPWA(withMDX(nextConfig));
