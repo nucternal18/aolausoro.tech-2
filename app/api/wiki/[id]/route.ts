574f25fef2993b707f057bae20ca9720fa899a88
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2024-06-16 17:21:47 +0100

    chore: Remove unnecessary code in api/wiki route

--- app/api/wiki/[id]/route.ts
+++ app/api/wiki/[id]/route.ts
@@ -2,29 +2,6 @@ import { NextResponse, NextRequest } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
 import prisma from "@lib/prismadb";
 
-export async function UPDATE(req: NextRequest) {
-  const { userId } = getAuth(req);
-
-  if (!userId) {
-    return new Response("Unauthorized", { status: 401 });
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { clerkId: userId },
-  });
-
-  if (!user?.isAdmin) {
-    return new Response(
-      "Not Authorized. You do not have permission to perform this operation.",
-      {
-        status: 401,
-      },
-    );
-  }
-
-  return NextResponse.json({ message: "Hello World" });
-}
-
 export async function DELETE(req: NextRequest) {
   const { userId } = getAuth(req);
 
