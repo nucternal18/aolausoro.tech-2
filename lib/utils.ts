312afa31d7193c51b7430b85e1290b86572ccb01
Adewoyin Oladipupo-Usoro <adewoyin@aolausoro.tech>
2024-08-31 18:35:29 +0100

    chore: commented-out fixedForwardRef function in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,11 +16,11 @@ export function formatDate(input: string | number): string {
   });
 }
 
-export const fixedForwardRef = <T, P = {}>(
-  render: (props: P, ref: React.Ref<T>) => React.ReactNode,
-): ((props: P & React.RefAttributes<T>) => React.ReactNode) => {
-  return React.forwardRef(render) as any;
-};
+// export const fixedForwardRef = <T, P = {}>(
+//   render: (props: P, ref: React.Ref<T>) => React.ReactNode,
+// ): ((props: P & React.RefAttributes<T>) => React.ReactNode) => {
+//   return React.forwardRef(render);
+// };
 
 export function formatBytes(
   bytes: number,
